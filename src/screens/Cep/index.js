1e3dc8ba7e91fa3fc60dac40e3d8fa187a701713
Luan <luanfrancavieira@gmail.com>
2020-02-20 21:31:19 -0300

    show information of cep

--- src/screens/Cep/index.js
+++ src/screens/Cep/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import {Body, Report, ReportTitle, ReportContent} from './styled'
 
 export default ({route, navigation}) => 
 {
-  console.log(route.params.dataCep)
+  const cep = route.params.dataCep
+
   return (
-    <View>
-      <Text>
-        CEP ENCONTRADO
-      </Text>
-    </View>
+    <Body>
+      <Report>
+        <ReportTitle>cep</ReportTitle>
+        <ReportContent>{cep.cep}</ReportContent>
+      </Report>
+
+      <Report>
+        <ReportTitle>estado</ReportTitle>
+        <ReportContent>{cep.uf}</ReportContent>
+      </Report>
+
+      <Report>
+        <ReportTitle>local/cidade</ReportTitle>
+        <ReportContent>{cep.localidade}</ReportContent>
+      </Report>
+
+      <Report>
+        <ReportTitle>bairro</ReportTitle>
+        <ReportContent>{cep.bairro}</ReportContent>
+      </Report>
+
+      <View>
+        <ReportTitle>logradouro</ReportTitle>
+        <ReportContent>{cep.logradouro}</ReportContent>
+      </View>
+    </Body>
   )
 }
---
+++ src/screens/Cep/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import {View, Text} from 'react-native'
+
+export const Body = styled(View)`
+    padding: 10% 5%;
+`
+
+export const Report = styled(View)`
+    margin-bottom: 12px;
+`
+
+export const ReportTitle = styled(Text)`
+    font-size: 14px;
+    text-transform: uppercase;
+    color: #A4A4A4;
+`
+
+export const ReportContent = styled(Text)`
+    font-size: 22px;
+`
